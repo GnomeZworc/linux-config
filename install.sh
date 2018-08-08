c0d1c5ea8a1ccaa4900b5da0a5d70d7f995dad85
GnomeZworc <nicolas.boufidjeline@gmail.com>
2018-08-09 00:58:36 +0200

    Add docker-compose

--- install.sh
+++ install.sh
@@ -44,3 +44,7 @@ add-apt-repository \
 	stable"
 apt-get update
 install docker-ce -y 2> ${trash_file} > ${trash_file}
+usermod -aG docker ${user}
+echo "Install docker-compose"
+curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
